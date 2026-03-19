5dc099a7b79a9afb7b889c526440d9b4ad71603c
Ziyado Hao <ziyadohao@gmail.com>
2026-03-19 16:18:41 +1100

    161819032026

--- Konec TBS.py
+++ Konec TBS.py
@@ -6,7 +6,7 @@ st.markdown("""
 <style>
             
 .stApp {
-    background-image: url("https://raw.githubusercontent.com/ziyadohao-lab/Konec-TBS/blob/main/Background.png");
+    background-image: url("https://www.google.com/imgres?q=photo&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1508921912186-1d1a45ebb3c1%3Ffm%3Djpg%26q%3D60%26w%3D3000%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%253D&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fphoto&docid=AgAT_yR1X6xrJM&tbnid=-gICFkUsIVmdYM&vet=12ahUKEwiPp-2Jm6uTAxWql68BHYtnKI8QnPAOegQIIBAB..i&w=3000&h=4500&hcb=2&ved=2ahUKEwiPp-2Jm6uTAxWql68BHYtnKI8QnPAOegQIIBAB");
     background-size: cover;        /* 铺满 */
     background-position: center;   /* 居中 */
     background-repeat: no-repeat;  /* 不重复 */
